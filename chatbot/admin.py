a1dbd76b33b9f6cc5667c2d57f650de2d1081ac3
lsrock1 <lsrock1@naver.com>
2016-09-30 09:42:12 +0000

    add db

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Subway,User,Schedule,Data
 # Register your models here.
+
+admin.site.register(Subway)
+admin.site.register(Schedule)
+admin.site.register(User)
+admin.site.register(Data)
--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+class Subway(models.Model):
+    line_name=models.CharField(max_length=20)
+    station_name=models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.station_name
+        
+class Schedule(models.Model):
+    date=models.DateField()
+    time=models.DateTimeField()
+    subway=models.ForeignKey(Subway)
+    weather=models.CharField(max_length=20)
+    interval=models.IntegerField()
+    
+    def __str__(self):
+        return self.date+self.time
+
+class User(models.Model):
+    user_key=models.CharField(max_length=250)
+    stacked_data=models.IntegerField()
+    
+    def __str__(self):
+        return self.user_key
+    
+class Data(models.Model):
+    schedule=models.ForeignKey(Schedule)
+    user=models.ForeignKey(User)
+    date=models.DateField(auto_now_add=True)
+    time=models.DateTimeField(auto_now_add=True)
+    
+    SIT_IN_DATA_CHOICES = (
+        (1, '입석'),
+        (2, '착석')
+    )
+    DETAIL_IN_DATA_CHOICES=(
+        (1,'콩나물 시루다'),
+        (2,'자리는 없다'),
+        (3,'자리가 남아 있다')
+        )
+    sit=models.IntegerField(choices=SIT_IN_DATA_CHOICES)
+    detail=models.IntegerField(choices=DETAIL_IN_DATA_CHOICES)
+    
+    def __str__(self):
+        return self.date+self.time
+
+    
